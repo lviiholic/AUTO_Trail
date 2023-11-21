d737a1f16fcf34402d9a4464b81f6c014a18db34
legendoflink <1226631013@qq.com>
2023-11-21 11:31:49 +0800

    markdown网页

--- mdT.py
+++ mdT.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # 读取Markdown文件内容
-    with open('example.md', 'r', encoding='utf-8') as f:
-        md_content = f.read()
-    # 使用Python-Markdown库将Markdown转换为HTML
-    html_content = markdown.markdown(md_content)
     # 将HTML内容传递给模板
-    return render_template('index.html', content=html_content)
+    return render_template('text.html')
+
+app.run(debug=True)
